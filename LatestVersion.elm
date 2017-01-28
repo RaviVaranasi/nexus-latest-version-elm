be6276cca770ae684232a7f6a20d72a089f13025
Ravi Varanasi <r_varanasi@containerstore.com>
2017-01-28 17:26:01 -0600

    Elm formatting

--- LatestVersion.elm
+++ LatestVersion.elm
@@ -8,33 +8,41 @@ import Json.Decode exposing (..)
 
 -- Model
 
-type alias Model = {
-     searchTerm : String
-     , results : List SearchResult
-}
 
-type alias SearchResult = {
-    groupId : String,
-    artifactId : String,
-    version : String
-}
+type alias Model =
+    { searchTerm : String
+    , results : List SearchResult
+    }
 
-init : (Model, Cmd Msg)
-init = ( { searchTerm = "", results = [] }, Cmd.none )
 
-type Msg = ChangeSearchTerm String
-         | PerformSearch
-         | SearchComplete (Result Http.Error (List SearchResult))
+type alias SearchResult =
+    { groupId : String
+    , artifactId : String
+    , version : String
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { searchTerm = "", results = [] }, Cmd.none )
+
+
+type Msg
+    = ChangeSearchTerm String
+    | PerformSearch
+    | SearchComplete (Result Http.Error (List SearchResult))
+
 
 
 -- View
+
 
 view : Model -> Html Msg
 view model =
     div []
         [ input [ onInput ChangeSearchTerm ] []
         , button [ onClick PerformSearch ] [ text "Search" ]
-        , ul [] ( List.map viewSearchResult model.results )
+        , ul [] (List.map viewSearchResult model.results)
         ]
 
 
@@ -44,43 +52,56 @@ viewSearchResult result =
         [ span [] [ text (toString result.version) ]
         ]
 
+
+
 -- update
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         ChangeSearchTerm newTerm ->
-             ( { model | searchTerm = newTerm }, Cmd.none )
+            ( { model | searchTerm = newTerm }, Cmd.none )
+
         PerformSearch ->
-             ( model, issueSearch model.searchTerm )
+            ( model, issueSearch model.searchTerm )
+
         SearchComplete (Ok results) ->
-             ( { model | results = results }, Cmd.none )
+            ( { model | results = results }, Cmd.none )
+
         SearchComplete (Err _) ->
-             ( model, Cmd.none )
+            ( model, Cmd.none )
+
 
 
 -- Subscriptions
 
+
 subscriptions : Model -> Sub Msg
-subscriptions model = Sub.none
+subscriptions model =
+    Sub.none
+
 
 
 -- HTTP
 
+
 issueSearch : String -> Cmd Msg
 issueSearch criteria =
     let
-        url = "http://nexus.containerstore.com/nexus/service/local/lucene/search?a="
-            ++ criteria
+        url =
+            "http://nexus.containerstore.com/nexus/service/local/lucene/search?a="
+                ++ criteria
+
         request =
-            {
-                verb = "GET",
-                headers = [("Accept", "application/json")],
-                url = url,
-                body = ""
+            { verb = "GET"
+            , headers = [ ( "Accept", "application/json" ) ]
+            , url = url
+            , body = ""
             }
     in
         Http.send SearchComplete (Http.get url decodeResults)
+
 
 decodeResult : Decoder SearchResult
 decodeResult =
@@ -89,16 +110,20 @@ decodeResult =
         (field "artifactId" string)
         (field "version" string)
 
+
 decodeResults : Decoder (List SearchResult)
-decodeResults = list decodeResult
+decodeResults =
+    list decodeResult
+
 
 
 -- Main
 
+
 main =
-    program {
-        init = init,
-        view = view,
-        update = update,
-        subscriptions = subscriptions
-    }
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
